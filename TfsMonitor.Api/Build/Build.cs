d6f53500c6149f5308c7ee1331e69749eee428ed
aaron.greenwald <aaron.greenwald@americancapital.com>
2014-09-04 21:45:51 -0400

    changing gitignore to not ignore contents of the Build folder in TfsMonitor.Api project, and adding the code files

---
+++ TfsMonitor.Api/Build/Build.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace TfsMonitor.Api.Build
+{
+	public class Build : ICloneable
+	{
+
+		private DateTime? _FinishedTime;
+		private string _Status;
+
+		public Uri BuildUri { get; set; }
+		public Uri BuildDefinitionUri { get; set; }
+		public string Project { get; set; }
+		public string Definition { get; set; }
+
+		public bool Completed { get; set; }
+		/// <summary>
+		/// GET will be null if Completed is false
+		/// </summary>				
+		public DateTime? FinishedTime
+		{
+			get { return (Completed ? _FinishedTime : null); }
+			set { _FinishedTime = value; }
+		}
+		public DateTime StartTime { get; set; }
+		/// <summary>
+		/// For builds still running, reflects the current runtime when accessed. Clients should ideally perform their own calculations against StartTime
+		/// </summary>
+		public Nullable<TimeSpan> RunTime
+		{
+			get
+			{
+				return (Completed ? (Nullable<TimeSpan>)(_FinishedTime - StartTime) : null);
+			}
+		}
+
+		public string Status
+		{
+			get { return _Status; }
+			set
+			{
+				string status = null;
+				switch (value)
+				{
+					case "Succeeded":
+						status = "Succeeded";
+						break;
+					case "InProgress":
+						status = "In Progress";
+						break;
+					case "Failed":
+						status = "Failed";
+						break;
+					default:
+						status = value;
+						break;
+				}
+				_Status = status;
+			}
+		}
+		/// <summary>
+		/// Reflects the Status field
+		/// </summary>
+		public bool Succeeded { get { return Status == "Succeeded"; } }
+		/// <summary>
+		/// Reflects the Status field
+		/// </summary>
+		public bool Failed { get { return Status == "Failed"; } }
+		/// <summary>
+		/// Reflects the Status field
+		/// </summary>
+		public bool InProgress { get { return Status == "In Progress"; } }
+
+		public string Username { get; set; }
+
+		public bool CIBuild { get; set; }
+		public bool ScheduledBuild { get; set; }
+		public bool ManualBuild { get; set; }
+
+		public enum BuildTrigger { CIBuild, ScheduledBuild, ManualBuild }
+
+		/// <summary>
+		/// Overridden for debugging convenience
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			return Definition + " - " + Status;
+		}
+
+		/// <summary>
+		/// If all CanRead properties on this Build equal the corresponding properties on the comparison object, 
+		/// they are considered equal. The properties' implemented Equals method will be used.
+		/// </summary>
+		/// <param name="comparison"></param>
+		/// <returns></returns>
+		public override bool Equals(object comparison)
+		{
+			var properties = this.GetType().GetProperties();
+			foreach (var propertyInfo in properties)
+			{
+				if (propertyInfo.CanRead)
+				{
+					if (!object.Equals(propertyInfo.GetValue(this), propertyInfo.GetValue(comparison)))
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		/// <summary>
+		/// Returns copy of the Build object, only the CanWrite properties are set. It's not a deep copy - the reference types retain references.			
+		/// </summary>
+		/// <returns></returns>
+		public object Clone()
+		{
+			Build cloned = new Build();
+			var properties = this.GetType().GetProperties();
+			foreach (var propertyInfo in properties)
+			{
+				if (propertyInfo.CanWrite)
+				{
+					propertyInfo.SetValue(cloned, propertyInfo.GetValue(this));
+				}
+			}
+			return cloned;
+		}
+
+	}
+
+}
---
+++ TfsMonitor.Api/Build/BuildMonitor.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.TeamFoundation.Build.Client;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TfsMonitor.Api.Build
+{
+	public class BuildMonitor
+	{		
+		private List<Build> BuildStatuses = new List<Build>();
+		private void UpdateBuildStatus()
+		{
+			string projectCollectionUrl = System.Configuration.ConfigurationManager.AppSettings["projectCollectionUrl"].ToString();
+			TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(new Uri(projectCollectionUrl));
+			var teamProjects = tfs.GetService<VersionControlServer>().GetAllTeamProjects(true);
+			IBuildServer buildServer = (IBuildServer)tfs.GetService(typeof(IBuildServer));
+
+			//todo perhaps we can use this and def.LastBuildUri together to speed up the process?
+			//def.LastBuildUri only contains completed builds, but is much faster than the query
+			//List<Uri> runningBuildDefinitions = new List<Uri>();
+			//IQueuedBuildSpec qbSpec = buildServer.CreateBuildQueueSpec("*", "*");
+			//var queuedBuilds = buildServer.QueryQueuedBuilds(qbSpec);
+			//foreach (var build in queuedBuilds.QueuedBuilds)
+			//{
+			//	runningBuildDefinitions.Add(build.BuildDefinitionUri);
+			//	Build b = new Build()
+			//	{
+
+			//	};
+			//}
+
+			List<Uri> newUris = new List<Uri>();
+			foreach (TeamProject proj in teamProjects)
+			{
+				var defs = buildServer.QueryBuildDefinitions(proj.Name);
+				foreach (IBuildDefinition def in defs)
+				{
+					IBuildDetailSpec spec = buildServer.CreateBuildDetailSpec(proj.Name, def.Name);
+					spec.QueryOptions = QueryOptions.None;
+					spec.QueryDeletedOption = QueryDeletedOption.ExcludeDeleted;
+					spec.InformationTypes = new string[] { };
+					spec.MaxBuildsPerDefinition = 1;
+					spec.QueryOrder = BuildQueryOrder.FinishTimeDescending;
+
+					var builds = buildServer.QueryBuilds(spec);
+					if (builds.Builds.Length > 0)
+					{
+						var buildDetail = builds.Builds[0];
+						if (!BuildStatuses.Exists(x => x.Completed && x.BuildUri == buildDetail.Uri))
+						{
+							newUris.Add(buildDetail.Uri);
+						}
+					}
+				}
+
+			}
+			if (newUris.Count > 0)
+			{
+				var builds = buildServer.QueryBuildsByUri(newUris.ToArray(), new string[] { }, QueryOptions.All);
+
+				foreach (IBuildDetail buildDetail in builds)
+				{
+					BuildStatuses.Remove(BuildStatuses.Where(x => x.BuildDefinitionUri == buildDetail.BuildDefinitionUri).FirstOrDefault());
+					Build build = new Build()
+					{
+						BuildUri = buildDetail.Uri,
+						BuildDefinitionUri = buildDetail.BuildDefinitionUri,
+						Definition = buildDetail.BuildDefinition.Name,
+						Project = buildDetail.TeamProject.ToString(),
+						Completed = buildDetail.BuildFinished,
+						StartTime = buildDetail.StartTime,
+						FinishedTime = buildDetail.FinishTime,
+						Status = buildDetail.Status.ToString(),
+						Username = buildDetail.RequestedBy
+
+					};
+					BuildStatuses.Add(build);
+				}
+			}
+
+		}
+
+		/// <summary>
+		/// Returns an updated copy of the list of builds (not status reference to the original - 
+		/// this facilitates keeping the returned list around long enough to compare it to the list
+		/// when it's returned some time in the future)
+		/// </summary>
+		/// <returns></returns>
+		public List<Build> GetBuildStatuses()
+		{
+			UpdateBuildStatus();
+
+			List<Build> result = new List<Build>();
+			result.AddRange(BuildStatuses.Select(x => (Build)x.Clone()));
+			return result;
+		}
+
+
+	}
+}
