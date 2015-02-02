0021a77c7fd8f2bef66b2d30a6ddb818903b2687
Jeff Martin <jeff.martin@americancapital.com>
2015-02-02 17:45:46 -0500

    Added Bing background images

---
+++ TfsMonitor.Web/Controllers/BingBackgroundsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace TfsMonitor.Controllers
+{
+    public class BingBackgroundsController : ApiController
+    {
+		public JObject Get() {
+			WebRequest request = WebRequest.Create("http://www.bing.com/HPImageArchive.aspx?format=js&idx=1&n=1&mkt=en-US");
+			WebResponse response = request.GetResponse();
+			StreamReader sr = new StreamReader(response.GetResponseStream());
+			return JObject.Parse(sr.ReadToEnd());
+		}
+	}
+}
--- TfsMonitor.Web/Views/Home/Home.Controller.js
+++ TfsMonitor.Web/Views/Home/Home.Controller.js
@@ -1,12 +1,30 @@
-﻿tfsMonitor.controller('home-controller', ['$scope', '$state', function ($scope, $state) {
+﻿tfsMonitor.controller('home-controller', ['$scope', '$state', '$http', function ($scope, $state, $http) {
 	$scope.home = new function () {
 		var utilities = {
-
+			loadBingBackgrounds: function () {
+				$http({
+					method: 'GET',
+					//the n= parameter can be changed to include images from prior days that could be rotated (maybe in the future)
+					url: 'api/BingBackgrounds'
+				})
+				.success(function (data) {
+					if (data.images && data.images.length) {
+						api.backgroundImage = (tfsMonitor.isSsl ? 'https' : 'http') + '://www.bing.com' + data.images[0].url
+					}
+				})
+			},
+			initialize: function () {
+				if (tfsMonitor.bingBackgrounds != null && Boolean(tfsMonitor.bingBackgrounds)) {
+					utilities.loadBingBackgrounds()
+				}
+			}
 		}
 
 		var api = {
-		
+			backgroundImage: null
 		}
+
+		utilities.initialize()
 
 		return api
 	}
